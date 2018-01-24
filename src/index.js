960210f657cc9b5f19a427204033989a8dfa1a80
Khinenw <deu07115@gmail.com>
2018-01-24 16:13:59 +0900

    Changed to react term

--- src/index.js
+++ src/index.js
@@ -17,51 +17,72 @@ export function reduceUI(state, { type, config }) {
 	return state
 };
 
-export const middleware = store => next => action => {
-	if (action.type !== 'SESSION_PTY_DATA') {
-		next(action);
-		return;
-	}
-
-	const config = store.getState().ui.powerline;
-	const terminal = getTerminal(config.terminal);
-
-	const startupMatch = terminalMatcher(action.data, terminal.startupRegex);
-	const promptMatch = terminalMatcher(action.data, terminal.promptRegex);
-
-	if(promptMatch || startupMatch) {
-		(async () => {
-			let directory = undefined;
-
-			if(promptMatch) {
-				directory = promptMatch[1];
-
-				let powerline = await buildPowerline(config.prompt, {
-					config,
-					directory,
-					terminal
-				});
-
-				action.data = action.data.replace(terminal.promptRegex, powerline);
-			}
-
-			if(startupMatch) {
-				if(startupMatch[1]) directory = startupMatch[1];
-
-				const startupPowerline = await buildPowerline(config.startup, {
-					config,
-					directory,
-					terminal
-				});
-
-				action.data = action.data.replace(terminal.startupRegex, startupPowerline);
-			}
-
-			next(action);
-		})();
-
-		return;
-	}
-
-	next(action);
+export function mapTermsState (state, map) {
+	return Object.assign(map, {
+		powerline: state.ui.powerline
+	});
 };
+
+export function decorateTerm(Term, {React}) {
+	return class extends React.Component {
+		constructor (props, context) {
+			super(props, context);
+
+			this._onData = this._onData.bind(this);
+		}
+
+		_onData(data) {
+			const next = (data) => {
+				if (this.props.onData) this.props.onData(data);
+			};
+
+			const config = this.props.powerline;
+			const terminal = getTerminal(config.terminal);
+
+			const startupMatch = terminalMatcher(data, terminal.startupRegex);
+			const promptMatch = terminalMatcher(data, terminal.promptRegex);
+
+			if(promptMatch || startupMatch) {
+				(async () => {
+					let directory = undefined;
+
+					if(promptMatch) {
+						directory = promptMatch[1];
+
+						let powerline = await buildPowerline(config.prompt, {
+							config,
+							directory,
+							terminal
+						});
+
+						data = data.replace(terminal.promptRegex, powerline);
+					}
+
+					if(startupMatch) {
+						if(startupMatch[1]) directory = startupMatch[1];
+
+						const startupPowerline = await buildPowerline(config.startup, {
+							config,
+							directory,
+							terminal
+						});
+
+						data = data.replace(terminal.startupRegex, startupPowerline);
+					}
+
+					next(data);
+				})();
+
+				return;
+			}
+
+			next(data);
+		}
+
+		render () {
+			return React.createElement(Term, Object.assign({}, this.props, {
+				onData: this._onData
+			}));
+		}
+	}
+}
